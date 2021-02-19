882547879babbb7bb882ff71c4e242385217d2bd
Amanda Simmons <carpe737diem@gmail.com>
2021-02-19 12:02:54 -0500

    successfully customized admin panel

--- custom_user_app/admin.py
+++ custom_user_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from custom_user_app.models import MyCustomUser
 
 # Register your models here.
+
+class MyCustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None, {'fields': ['username']}),
+        (None, {'fields': ['password']}),
+        (None, {'fields': ['display_name']}),
+        (None, {'fields': ['age']}),
+    ]
+
+admin.site.register(MyCustomUser, MyCustomUserAdmin)
