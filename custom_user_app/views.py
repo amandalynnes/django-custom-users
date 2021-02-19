a4cb33a4e4e14ef8187cf261af990332a348bca2
Amanda Simmons <carpe737diem@gmail.com>
2021-02-19 13:20:35 -0500

    added auth_user_model to display

--- custom_user_app/views.py
+++ custom_user_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from custom_user_app.models import MyCustomUser
+from django.contrib.auth import settings
 # from custom_user_app.forms import AddCustomUser
 
 
@@ -11,10 +12,12 @@ from custom_user_app.models import MyCustomUser
 def index_view(request):
     username = MyCustomUser.objects.get().username
     display_nm = MyCustomUser.objects.get().Display_name
+    auth_user_model = settings.AUTH_USER_MODEL
 
     return render(request, "index.html", {
         "heading": "Logged-in Users!",
         "username": username,
         "display_nm": display_nm,
+        "auth_user_model": auth_user_model,
     })
 
